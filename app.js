ae81db76f97a37385a7401d3482d71ca4aaf8356
SaiKumar <saikumarmysa31@gmail.com>
2024-06-19 07:22:16 +0530

    initial commit

--- app.js
+++ app.js
@@ -7,19 +7,33 @@ const server = app.listen(PORT, ()=>{
 })
 const io = require('socket.io')(server);
 app.use(express.static(path.join(__dirname,'public')));
+//To store socket id and name
+let socketsConnected = new Map();
 
-const socketsConnected = new Set();
 io.on('connection',onConnected)
+
 function onConnected(socket){
     console.log('User Connected',socket.id);
-    socketsConnected.add(socket.id);
-    io.emit('client-total',socketsConnected.size)
-    socket.on('disconnect',function(){
-    console.log('User Disconnected')
-    socketsConnected.delete(socket.id);
-    io.emit('client-total',socketsConnected.size)
-    })
 
+    socket.on('new-user-joined',username =>{
+        console.log('New User: '+username);
+        //setting up socketid , username in socketsConnected
+        socketsConnected.set(socket.id, username);
+        socket.broadcast.emit('new-user-joined',username)
+        io.emit('clients-total',socketsConnected.size)
+    });
+
+    socket.on('disconnect',() => {
+        const uname = socketsConnected.get(socket.id);
+        if(uname){
+            console.log('User Disconnected: '+uname);
+            //notifying others that user has left
+            socket.broadcast.emit('user-left',uname);
+
+            socketsConnected.delete(socket.id);
+            io.emit('clients-total',socketsConnected.size)
+        }
+    })
 
 socket.on('message',(data)=>{
     //console.log(data);
--- public/main.js
+++ public/main.js
@@ -1,28 +1,46 @@
 const socket = io();
 const clientsTotal = document.getElementById('client-total')
-
+const username = prompt('Enter your name to join chat!')
 const messageContainer = document.getElementById('message-container');
-console.log('messageContainer'+messageContainer)
-const nameInput = document.getElementById('name-input')
+const nameInput = document.getElementById('name-input').value = username;
 const messageForm = document.getElementById('message-form');
 const messageInput = document.getElementById('message-input')
-
 const messageTone = new Audio('/happy-pop.mp3')
+messageContainer.innerHTML = '';
+
+socket.emit('new-user-joined',username);
+
+// Listener for user-joined the chat
+socket.on('new-user-joined',username =>{
+    append(`${username} joined the chat`, 'right')
+})
+
+// Listener for user-left the chat
+socket.on('user-left',uname => {
+    append(`${uname} left the chat`, 'left')
+})
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add('position');
+    messageContainer.append(messageElement)
+};
 
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     sendMessage()
 })
 
-socket.on('client-total',(data)=>{
+socket.on('clients-total',(data)=>{
     clientsTotal.innerText = `Users Online: ${data}`  
 })
 
 function sendMessage(){
     if(messageInput.value=='') return
-    //console.log(messageInput.value)
     const data = {
-        name: nameInput.value,
+        name: nameInput,
         message: messageInput.value,
         dateTime: new Date(),
     }
@@ -30,10 +48,10 @@ function sendMessage(){
     addMessageToUI(true,data);
     messageInput.value = ''
 }
+
 socket.on('chat-message',(data)=>{
     messageTone.play();
     //console.log(data);
-    //here this message is not our own message its a incoming mesg to other user
     addMessageToUI(false,data);
 })
 
@@ -46,26 +64,28 @@ function addMessageToUI(isOwnMessage, data) {
               <span>${data.name}  ${moment(data.dateTime).fromNow()}</span>
             </p>
           </li>`  
-        messageContainer.innerHTML +=element;
-        scrollToBottom();
-        
-  }
+    messageContainer.innerHTML +=element;
+    scrollToBottom();    
+}
+
 //function for Automatic-scrolldown
 function scrollToBottom(){
 messageContainer.scrollTo(0,messageContainer.scrollHeight);
 }
 
 //Event-listeners:
+
 //if someone points on message input field
 messageInput.addEventListener('focus',(e)=>{
     socket.emit('feedback',{
-        feedback:`${nameInput.value} is typing a message...`
+        feedback:`${nameInput} is typing a message...`
     })
 })
+
 //if someone is typing/ pressing keys
 messageInput.addEventListener('keypress',(e)=>{
     socket.emit('feedback',{
-        feedback:`${nameInput.value} is typing a message...`
+        feedback:`${nameInput} is typing a message...`
     })
 })
 
