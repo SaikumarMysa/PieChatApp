7a5a61e1281ae192afe1a5b8b5b8ee99c8085524
SaiKumar <saikumarmysa31@gmail.com>
2024-06-20 10:20:15 +0530

    initial commit

--- app.js
+++ app.js
@@ -7,39 +7,34 @@ const server = app.listen(PORT, ()=>{
 })
 const io = require('socket.io')(server);
 app.use(express.static(path.join(__dirname,'public')));
+
 //To store socket id and name
 let socketsConnected = new Map();
-
 io.on('connection',onConnected)
-
 function onConnected(socket){
-    console.log('User Connected',socket.id);
+console.log('User Connected',socket.id);
+socket.on('new-user-joined',username =>{
+console.log('New User: '+username);
 
-    socket.on('new-user-joined',username =>{
-        console.log('New User: '+username);
-        //setting up socketid , username in socketsConnected
-        socketsConnected.set(socket.id, username);
-        socket.broadcast.emit('new-user-joined',username)
-        io.emit('clients-total',socketsConnected.size)
-    });
+//setting up socketid , username in socketsConnected
+socketsConnected.set(socket.id, username);
+socket.broadcast.emit('new-user-joined',username)
+io.emit('clients-total',socketsConnected.size)
+});
+socket.on('disconnect',() => {
+const uname = socketsConnected.get(socket.id);
+if(uname){
+console.log('User Disconnected: '+uname);
 
-    socket.on('disconnect',() => {
-        const uname = socketsConnected.get(socket.id);
-        if(uname){
-            console.log('User Disconnected: '+uname);
-            //notifying others that user has left
-            socket.broadcast.emit('user-left',uname);
-
-            socketsConnected.delete(socket.id);
-            io.emit('clients-total',socketsConnected.size)
-        }
-    })
-
+//notifying others that user has left
+socket.broadcast.emit('user-left',uname);
+socketsConnected.delete(socket.id);
+io.emit('clients-total',socketsConnected.size)
+}
+})
 socket.on('message',(data)=>{
-    //console.log(data);
     socket.broadcast.emit('chat-message',data)
 })
-
 socket.on('feedback',(data)=>{
     socket.broadcast.emit('feedback',data)
 })
